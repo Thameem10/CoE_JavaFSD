de1e498608a472f21b2bcc4e180736285a7c24fb
Thameem10 <138700287+Thameem10@users.noreply.github.com>
2025-04-01 16:50:52 +0530

    Delete Angular_Assignment/ShopEase/src directory

--- Angular_Assignment/ShopEase/src/app/guards/enquiry.guard.ts
+++
@@ -1,6 +0,0 @@
-import { CanActivateFn } from '@angular/router';
-
-export const enquiryGuard: CanActivateFn = (route, state) => {
-  const username = localStorage.getItem('username');
-  return username?true:false;
-};
